fc88675a4258e745b59297370ee961b739e02078
Philip <seongju.lee90@gmail.com>
2022-09-01 16:32:06 -0700

    drop database to refresh

--- backend/routes/api/reviewimage.js
+++ backend/routes/api/reviewimage.js
@@ -9,7 +9,28 @@ const router = express.Router();
 
 router.delete('/:imageId', requireAuth, async(req, res, next) => {
     const userId = req.user.id;
-    const imageId = req.params.imageId
+    const imageId = req.params.imageId;
+
+    const reviewImage = await ReviewImage.findByPk(imageId)
+    const review = await Review.findByPk(userId)
+
+    // Error response: Couldn't find a Review Image with the specified id
+    if (!reviewImage) {
+        res.status(404)
+        res.json({
+            "message": "Review Image couldn't be found",
+            "statusCode": 404
+        })
+    }
+
+    // Review must belong to the current user
+    if (userId === review.userId) {
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        })
+    }
+
 })
 
 module.exports = router;
