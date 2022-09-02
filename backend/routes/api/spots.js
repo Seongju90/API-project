f68e71ac4200668733903775868037f6d0d3dd7f
Philip <seongju.lee90@gmail.com>
2022-09-02 09:12:37 -0700

    comment out validations on spots because postgress doesn't like it

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -39,106 +39,108 @@ router.get('/', async(req, res, next) => {
             }
         })
     }
-    let parsedMinLat = parseInt(minLat)
-    let parsedMaxLat = parseInt(maxLat)
-    let parsedMinLng = parseInt(minLng)
-    let parsedMaxLng = parseInt(maxLng)
-    let parsedMinPrice = parseInt(minPrice)
-    let parsedMaxPrice = parseInt(maxPrice)
-    // create where query
-    const where = {};
-    // check if its an integer OR (lat - rounded lat not zero) === decimal OR doesnt exist (optional)
-    if (isNaN(parsedMinLat)) {
-        1 === 1; // do nothing
-    } else if (Number.isInteger(parsedMinLat) || (parsedMinLat - Math.floor(parsedMinLat)) !== 0) {
-        where.parsedMinLat = parsedMinLat
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "minLat": "Minimum latitude is invalid",
-            }
-        })
-    }
+    // @@@@@@@@ POSTGRESS WONT LET YOU STORE NaN as a value FIND DIFFERENT WAY @@@@@@@@@@@
 
-    if (isNaN(parsedMaxLat)) {
-        1 === 1; // do nothing
-    } else if (Number.isInteger(parsedMaxLat) || (parsedMaxLat - Math.floor(parsedMaxLat)) !== 0 || undefined) {
-        where.parsedMaxLat = parsedMaxLat
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "maxLat": "Maximum latitude is invalid",
-            }
-        })
-    }
+    // let parsedMinLat = parseInt(minLat)
+    // let parsedMaxLat = parseInt(maxLat)
+    // let parsedMinLng = parseInt(minLng)
+    // let parsedMaxLng = parseInt(maxLng)
+    // let parsedMinPrice = parseInt(minPrice)
+    // let parsedMaxPrice = parseInt(maxPrice)
+    // // create where query
+    // const where = {};
+    // // check if its an integer OR (lat - rounded lat not zero) === decimal OR doesnt exist (optional)
+    // if (isNaN(parsedMinLat)) {
+    //     1 === 1; // do nothing
+    // } else if (Number.isInteger(parsedMinLat) || (parsedMinLat - Math.floor(parsedMinLat)) !== 0) {
+    //     where.parsedMinLat = parsedMinLat
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "minLat": "Minimum latitude is invalid",
+    //         }
+    //     })
+    // }
 
-    if (isNaN(parsedMinLng)) {
-        1 === 1; // do nothing
-    } else if (Number.isInteger(parsedMinLng) || (parsedMinLng - Math.floor(parsedMinLng)) !== 0 || undefined) {
-        where.parsedMinLng = parsedMinLng
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "minLng": "Maximum longitude is invalid"
-            }
-        })
-    }
+    // if (isNaN(parsedMaxLat)) {
+    //     1 === 1; // do nothing
+    // } else if (Number.isInteger(parsedMaxLat) || (parsedMaxLat - Math.floor(parsedMaxLat)) !== 0 || undefined) {
+    //     where.parsedMaxLat = parsedMaxLat
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "maxLat": "Maximum latitude is invalid",
+    //         }
+    //     })
+    // }
 
-    if (isNaN(parsedMaxLng)) {
-        1 === 1; // do nothing
-    } else if (Number.isInteger(parsedMaxLng) || (parsedMaxLng - Math.floor(parsedMaxLng)) !== 0 || undefined) {
-        where.parsedMaxLng = parsedMaxLng
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "maxLng": "Minimum longitude is invalid"
-            }
-        })
-    }
+    // if (isNaN(parsedMinLng)) {
+    //     1 === 1; // do nothing
+    // } else if (Number.isInteger(parsedMinLng) || (parsedMinLng - Math.floor(parsedMinLng)) !== 0 || undefined) {
+    //     where.parsedMinLng = parsedMinLng
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "minLng": "Maximum longitude is invalid"
+    //         }
+    //     })
+    // }
 
-    if (isNaN(parsedMinPrice)) {
-        1 === 1; // do nothing
-    } else if ((Number.isInteger(parsedMinPrice) || (parsedMinPrice - Math.floor(parsedMinPrice)) !== 0 || undefined)
-        && parsedMinPrice >= 0) {
-        where.parsedMinPrice = parsedMinPrice
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "minPrice": "Minimum price must be greater than or equal to 0"
-            }
-        })
-    }
+    // if (isNaN(parsedMaxLng)) {
+    //     1 === 1; // do nothing
+    // } else if (Number.isInteger(parsedMaxLng) || (parsedMaxLng - Math.floor(parsedMaxLng)) !== 0 || undefined) {
+    //     where.parsedMaxLng = parsedMaxLng
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "maxLng": "Minimum longitude is invalid"
+    //         }
+    //     })
+    // }
 
-    if (isNaN(parsedMaxPrice)) {
-        1 === 1; // do nothing
-    } else if ((Number.isInteger(parsedMaxPrice) || (parsedMaxPrice - Math.floor(parsedMaxPrice)) !== 0 || undefined)
-        && parsedMaxPrice >= 0) {
-        where.parsedMaxPrice = parsedMaxPrice
-    } else {
-        res.status(400)
-        res.json({
-            "message": "Validation Error",
-            "statusCode": 400,
-            "errors": {
-                "maxPrice": "Maximum price must be greater than or equal to 0"
-            }
-        })
-    }
+    // if (isNaN(parsedMinPrice)) {
+    //     1 === 1; // do nothing
+    // } else if ((Number.isInteger(parsedMinPrice) || (parsedMinPrice - Math.floor(parsedMinPrice)) !== 0 || undefined)
+    //     && parsedMinPrice >= 0) {
+    //     where.parsedMinPrice = parsedMinPrice
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "minPrice": "Minimum price must be greater than or equal to 0"
+    //         }
+    //     })
+    // }
+
+    // if (isNaN(parsedMaxPrice)) {
+    //     1 === 1; // do nothing
+    // } else if ((Number.isInteger(parsedMaxPrice) || (parsedMaxPrice - Math.floor(parsedMaxPrice)) !== 0 || undefined)
+    //     && parsedMaxPrice >= 0) {
+    //     where.parsedMaxPrice = parsedMaxPrice
+    // } else {
+    //     res.status(400)
+    //     res.json({
+    //         "message": "Validation Error",
+    //         "statusCode": 400,
+    //         "errors": {
+    //             "maxPrice": "Maximum price must be greater than or equal to 0"
+    //         }
+    //     })
+    // }
 
     // find all spots with pagination
     let spots = await Spot.findAll({
