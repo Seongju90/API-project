2befce1f5f799dc69237fd7239d8317e93ea18a2
Philip <seongju.lee90@gmail.com>
2022-10-14 13:31:35 -0700

    front end heroku prep

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,18 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-// In this test route, you are setting a cookie on the response with the name of
-// XSRF-TOKEN to the value of the req.csrfToken method's return.
-// Then, you are sending the text, Hello World! as the response's body.
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-// backend/routes/index.js
 // Add a XSRF-TOKEN cookie
-
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
@@ -40,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -51,9 +40,17 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
   });
 }
 
