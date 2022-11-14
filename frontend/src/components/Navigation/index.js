bedbcbe11c027aa38d2b71e52c207fd063a92c0f
Philip <seongju.lee90@gmail.com>
2022-11-14 13:49:01 -0800

    stuck on dropdown

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,9 +6,16 @@ import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 import Ground from '../SVG/ground';
 import titleImage from './groundbnb.png';
+import { useState, useEffect } from "react";
 
 function Navigation({ isLoaded }){
     const sessionUser = useSelector(state => state.session.user);
+    const [showMenu, setShowMenu] = useState(false);
+
+    const openMenu = () => {
+      if (showMenu) return;
+      setShowMenu(true);
+    };
 
     let sessionLinks;
     if (sessionUser) {
@@ -37,7 +44,12 @@ function Navigation({ isLoaded }){
                 {/* conditionally render create spot, for non-users */}
                 { sessionUser && <NavLink to="/spots">Become a Host</NavLink>}
               </span>
-              {isLoaded && sessionLinks}
+              {/* {isLoaded && sessionLinks} */}
+              <button onClick={openMenu}>
+                <i class="fa-solid fa-bars"></i>
+                <i className="fas fa-user-circle" />
+              </button>
+              {showMenu && sessionLinks}
             </div>
         </div>
     );
