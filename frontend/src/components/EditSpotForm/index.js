390d8b421ae568ddd37be263b0dba66564297abd
Philip <seongju.lee90@gmail.com>
2022-11-17 13:38:52 -0800

    edit form css finished

--- frontend/src/components/EditSpotForm/index.js
+++ frontend/src/components/EditSpotForm/index.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getOneSpot, editASpot } from "../../store/spots"
 
+import './EditSpot.css';
+
 const EditSpotForm = ({setShowModal}) => {
     const dispatch = useDispatch();
 
@@ -62,6 +64,7 @@ const EditSpotForm = ({setShowModal}) => {
 
         // If I have any errors set them, otherwise edit the spot
         setErrors(error)
+        if (error.length) return;
 
         // might need spot & spotId, error message showing undefined
         // make edit wait before executing the hide modal
@@ -76,78 +79,100 @@ const EditSpotForm = ({setShowModal}) => {
         setShowModal(false)
     }
         return (
-            <form onSubmit={handleSubmit}>
+            <form className="edit-spot-main-container" onSubmit={handleSubmit}>
                 <h1>Edit a Spot</h1>
-                <ul>
-                    {errors.map((error, idx) => (
-                    <li key={idx}>{error}</li>
-                    ))}
-                </ul>
-                <label>
-                    Address
-                    <input
-                    type="text"
-                    value={address}
-                    onChange={e => setAddress(e.target.value)}
-                    />
-                </label>
-                <label>
-                    City
-                    <input
-                    type="text"
-                    value={city}
-                    onChange={e => setCity(e.target.value)}
-                    />
-                </label>
-                <label>
-                    State
-                    <input
-                    type="text"
-                    value={state}
-                    onChange={e => setState(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Country
-                    <input
-                    type="text"
-                    value={country}
-                    onChange={e => setCountry(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Name
-                    <input
-                    type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Description
-                    <input
-                    type="text"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Price
-                    <input
-                    type="number"
-                    value={price}
-                    onChange={e => setPrice(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Image url
-                    <input
-                    type="text"
-                    value={url}
-                    onChange={e => setUrl(e.target.value)}
-                    />
-                </label>
+                <div className="edit-spot-detail-container">
+                    <ul>
+                        {errors.map((error, idx) => (
+                        <li key={idx}>{error}</li>
+                        ))}
+                    </ul>
+                    <div className="editspot">
+                        <label className="label">
+                            Address
+                            <input
+                            className="input"
+                            type="text"
+                            value={address}
+                            onChange={e => setAddress(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="city-label-editspot">
+                            City
+                            <input
+                            className="city-input-editspot"
+                            type="text"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="state-label-editspot">
+                            State
+                            <input
+                            className="state-input-editspot"
+                            type="text"
+                            value={state}
+                            onChange={e => setState(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="country-label-editspot">
+                            Country
+                            <input className="country-input-editspot"
+                            type="text"
+                            value={country}
+                            onChange={e => setCountry(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="name-label-editspot">
+                            Name
+                            <input className="name-input-editspot"
+                            type="text"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="description-label-editspot">
+                            Description
+                            <input className="description-input-editspot"
+                            type="text"
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="price-label-editspot">
+                            Price
+                            <input className="price-input-editspot"
+                            type="number"
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="editspot">
+                        <label className="url-label-editspot">
+                            Image url
+                            <input className="url-input-editspot"
+                            type="text"
+                            value={url}
+                            onChange={e => setUrl(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                </div>
                 <button
+                    className="edit-button"
                     type="submit"
                 >
                     Submit
